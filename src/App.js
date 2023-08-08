88aef3f3c8cea551e1e24d217735b6f826fd276b
Ryan Adriaans <Ryaan.1996s@hotmail.com>
2023-08-08 17:17:34 -0400

    Presetup for Auth 2.0

--- src/App.js
+++ src/App.js
@@ -22,6 +22,10 @@ function App() {
   const [showInitial, setShowInitial] = useState(true);
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
+  const userID = () => {
+    
+  }
+
   const handleSearch = () => {
     if (searchQuery !== '') {
       Search(searchQuery, accessToken, setReturnedInfo);
